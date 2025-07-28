2b2adf316636cacff6f991f8c2645e683d902100
Kirill Severin <kirillseverin2020@gmail.com>
2025-07-28 17:17:52 +0300

    Squashed commit of the following:
    commit ef353c8a33cf5e10c652cb80168b648d1beb0c38
    Author: Kirill Severin <kirillseverin2020@gmail.com>
    Date:   Mon Jul 28 13:43:12 2025 +0300
    
        Deleting storyboard info
    
    commit 125dcca617425282de77593fab1b0a1571c1e400
    Author: Kirill Severin <kirillseverin2020@gmail.com>
    Date:   Mon Jul 28 13:15:09 2025 +0300
    
        Creating an empty project

---
+++ TodoList/TodoList/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Kirill Severin on 28.07.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
