125dcca617425282de77593fab1b0a1571c1e400
Kirill Severin <kirillseverin2020@gmail.com>
2025-07-28 13:15:09 +0300

    Creating an empty project

---
+++ TodoList/TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Kirill Severin on 28.07.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
